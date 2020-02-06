e0f6baa2fd0a10638a88ad955ca233c05e39105d
Joao Gabriel Palmeira Bonsegno <joaogpb@primeit.com.br>
2020-02-06 16:31:32 -0300

    Adição do projeto com HelloWorld e REST

---
+++ 01RestWithSpring/src/main/java/br/com/joao/GreetingController.java
@@ -0,0 +1,22 @@
+package br.com.joao;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	private static final String template = "Hello, %s";
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting") //Definição da rota
+	// Response do servidor fica dentro deste método
+	public Greeting getGreeting(@RequestParam(value="name", defaultValue="World") String name) {
+		// Os parâmetros da requisição ficam na annotation "@RequestParam"
+		// O atributo "value" define qual será o nome do parâmetro na URL
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
